42e20080670666ac7a4c1a1038e2e80a8f4823e8
ErinKace <erinkacerovskis@gmail.com>
2023-05-24 19:40:38 -0500

    finished studio but stuck on the bonus mission

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14;
+//        while(!input.hasNextDouble()) {
+//            System.out.println("Enter the radius of the circle: ");
+//        }
+        System.out.println("Enter the radius of the circle: ");
+
+        while (!input.hasNextDouble() || (input.nextDouble() < 0)) {
+            System.out.println("Please enter a positive number: ");
+        }
+        double radius = input.nextDouble();
+
+        input.close();
+
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle with radius "+radius+" is "+area);
+    }
+}
